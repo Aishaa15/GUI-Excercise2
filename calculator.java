f55f1d37b03afc699593ab5e280b11e91c65375e
Aisha <128395400+Aishaa15@users.noreply.github.com>
2023-04-15 17:20:32 +0000

    added a action listner

--- calculator.java
+++ calculator.java
@@ -33,6 +33,119 @@ public class calculator extends JFrame
         add(divideButton);
         add(resultLabel);
 
+        //add action listener to the add buttons
+        addButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                //decalare variables 
+                double num1, num2, result;
+                try {
+                        // Get the numbers from the text fields
+                        num1 = Double.parseDouble(num1Field.getText());
+                        num2 = Double.parseDouble(num2Field.getText());
+                    } 
+                    
+                    catch (NumberFormatException ex) 
+                    {
+                        resultLabel.setText("Error: Invalid input");
+                        return;
+                    }
+                
+                // Perform the calculation based on the button clicked
+                if (e.getSource() == addButton) 
+                {
+                    resultLabel.setText("Result: " + (num1 + num2));
+                } 
+            }
+        });
+        
+        //add action listener to the add buttons
+        subtractButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                //decalare variables 
+                double num1, num2, result;
+                try {
+                        // Get the numbers from the text fields
+                        num1 = Double.parseDouble(num1Field.getText());
+                        num2 = Double.parseDouble(num2Field.getText());
+                    } 
+                    
+                    catch (NumberFormatException ex) 
+                    {
+                        resultLabel.setText("Error: Invalid input");
+                        return;
+                    }
+                
+                // Perform the calculation based on the button clicked
+                if (e.getSource() == subtractButton) 
+                {
+                    resultLabel.setText("Result: " + (num1 - num2));
+                } 
+            }
+        });
+        
+        //add action listener to the add buttons
+        multiplyButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                //decalare variables 
+                double num1, num2, result;
+                try {
+                        // Get the numbers from the text fields
+                        num1 = Double.parseDouble(num1Field.getText());
+                        num2 = Double.parseDouble(num2Field.getText());
+                    } 
+                    
+                    catch (NumberFormatException ex) 
+                    {
+                        resultLabel.setText("Error: Invalid input");
+                        return;
+                    }
+                
+                // Perform the calculation based on the button clicked
+                if (e.getSource() == multiplyButton) 
+                {
+                    resultLabel.setText("Result: " + (num1 * num2));
+                } 
+            }
+        });
+        
+        //add action listener to the add buttons
+        divideButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                //decalare variables 
+                double num1, num2, result;
+                try {
+                        // Get the numbers from the text fields
+                        num1 = Double.parseDouble(num1Field.getText());
+                        num2 = Double.parseDouble(num2Field.getText());
+                    } 
+                    
+                    catch (NumberFormatException ex) 
+                    {
+                        resultLabel.setText("Error: Invalid input");
+                        return;
+                    }
+                
+                // Perform the calculation based on the button clicked
+                if (e.getSource() == divideButton) 
+                {
+                    resultLabel.setText("Result: " + (num1 / num2));
+                } 
+            }
+        });
+        
+        
+        // set window size and make it visible
+        setSize(500, 500);
+        setVisible(true);
+
     }
 
     
